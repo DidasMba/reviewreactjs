fb54a9c0dd8eb22834e74db5e31a1d19abe6d93a
DidasMba <daniel.mbarushi@gmail.com>
2023-12-21 23:42:37 +0300

    component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Article />
+      <Comment />
+    </div>
+  );
+}
+
+function Article()
+{
+  return(
+    <div> Dear</div>
+  )
+}
+
+function  Comment(){
+  return <div>
+    Naturally
+  </div>
+}
+export default App;
--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-function Article(){
-  return(
-    <div>Dear Reader: Hello</div>
-  )
-}
